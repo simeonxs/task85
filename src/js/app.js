dbfbac705185b14cb3104949c7c2df1c3604aa4b
simeonxs <119071613+simeonxs@users.noreply.github.com>
2022-11-26 01:05:24 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
-  const ul = document.querySelector("ul");
-});
+    // This block will be executed once the page is loaded and ready
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=10`) 
+    .then(res => res.json())
+    .then(data=> {
+      data.results.forEach(pok => {
+        let ul = document.createElement('ul')
+        ul.innerHTML = `<li>${pok.name}</li>`
+        document.body.appendChild(ul)
+        
+      })
+  })
+  
+    
+  });
